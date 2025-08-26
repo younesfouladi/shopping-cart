bc8f8bc694b01dd1959d0322d652d795227e45b2
Younes <mehrabtahami@gmail.com>
2025-08-26 15:40:58 +0330

    Changed Desktop Navbar To use Flyout Component for Categories

--- src/components/navbar/DesktopNavbar.tsx
+++ src/components/navbar/DesktopNavbar.tsx
@@ -1,37 +1,9 @@
 import { Link, NavLink } from "react-router-dom";
 import { ShoppingBag, UserRoundPen } from "lucide-react";
 import { gsap } from "gsap";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function DesktopNavbar() {
-  const catRef = useRef<HTMLUListElement | null>(null);
-
-  const showCats = () => {
-    if (!catRef.current) return;
-    gsap.killTweensOf(catRef.current);
-    gsap.fromTo(
-      catRef.current,
-      { display: "flex", scaleY: 0 },
-      { scaleY: 1, duration: 0.5 }
-    );
-  };
-
-  const hideCats = () => {
-    if (!catRef.current) return;
-    gsap.killTweensOf(catRef.current);
-    gsap.fromTo(
-      catRef.current,
-      { scaleY: 1 },
-      {
-        scaleY: 0,
-        duration: 0.5,
-        onComplete: () => {
-          if (catRef.current) catRef.current.style.display = "none";
-        },
-      }
-    );
-  };
-
   return (
     <div className="hidden lg:flex flex-col">
       <div className="flex justify-between items-center">
@@ -88,26 +60,70 @@ export default function DesktopNavbar() {
           </Link>
         </div>
       </div>
-      <div
-        className="group w-fit m-auto flex flex-col items-center justify-center cursor-pointer hover:w-full"
-        role="listitem"
-        onMouseLeave={hideCats}
-      >
-        <h3
-          className="group-hover:text-green-600 hover:w-full text-center pb-4"
-          onMouseEnter={showCats}
-        >
-          Category
-        </h3>
-        <ul
-          className="hidden flex-col w-full gap-4 items-center pt-4 origin-top"
-          ref={catRef}
-        >
-          <li>Shoes</li>
-          <li>Clothes</li>
-          <li>Shirts</li>
-        </ul>
-      </div>
+      <FlyoutMenu title={"Cateory"} links={["Shoes", "Clothes", "Shirts"]} />
     </div>
   );
 }
+
+interface IFlyoutMent {
+  title: string;
+  links: string[];
+}
+
+const FlyoutMenu = ({ title, links }: IFlyoutMent) => {
+  const catRef = useRef<HTMLUListElement | null>(null);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      if (!catRef.current) return;
+      gsap.killTweensOf(catRef.current);
+      gsap.fromTo(
+        catRef.current,
+        { display: "flex", scaleY: 0 },
+        { scaleY: 1, duration: 0.5 }
+      );
+    } else {
+      if (!catRef.current) return;
+      gsap.killTweensOf(catRef.current);
+      gsap.fromTo(
+        catRef.current,
+        { scaleY: 1 },
+        {
+          scaleY: 0,
+          duration: 0.5,
+          onComplete: () => {
+            if (catRef.current) catRef.current.style.display = "none";
+          },
+        }
+      );
+    }
+  }, [open]);
+
+  return (
+    <div
+      className="relative group w-fit m-auto flex flex-col items-center justify-center "
+      role="listitem"
+      onMouseLeave={() => setOpen(false)}
+    >
+      <a
+        className="group-hover:text-green-600 hover:w-full text-center pb-4 px-4"
+        onMouseEnter={() => setOpen(true)}
+        href=""
+      >
+        {title}
+      </a>
+      <ul
+        className="hidden absolute top-10 flex-col gap-4 items-center p-4 origin-top bg-green-600 text-neutral-50 rounded-xl"
+        ref={catRef}
+      >
+        <span className="absolute w-4 h-4 bg-green-600 rotate-45 -top-2"></span>
+        {links.map((item) => (
+          <li key={item}>
+            <NavLink to={" "}>{item}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
