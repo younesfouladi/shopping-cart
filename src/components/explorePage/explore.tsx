ae1575852b91632cd5deb1a8c11b79b7154261f1
Younes <mehrabtahami@gmail.com>
2025-08-27 18:22:07 +0330

    Style Explore Page

--- src/components/explorePage/explore.tsx
+++ src/components/explorePage/explore.tsx
@@ -1,21 +1,24 @@
 import { useOutletContext } from "react-router-dom";
 import { ProductCard } from "../Utilities";
 import type { IProductContext } from "../../App";
+import { LoadingSpinner } from "../Utilities";
 
 export default function ExplorePage() {
   const [products] = useOutletContext<IProductContext>();
-  console.log(products);
+
+  if (products.length === 0) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <div id="explore-container" className="pb-14">
       <section className="space-y-2 mb-4">
-        <h1 className="font-bold flex justify-between">
-          All Products<button className="text-green-700">see all</button>
-        </h1>
+        <h1 className="font-bold text-center">All Products</h1>
         <div
           id="all-products"
-          className="grid grid-cols-2 gap-4 lg:grid-cols-3 2xl:grid-cols-4"
+          className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5"
         >
-          {products.slice(0, 4).map((item) => (
+          {products.map((item) => (
             <ProductCard key={item.id} product={item} />
           ))}
         </div>
